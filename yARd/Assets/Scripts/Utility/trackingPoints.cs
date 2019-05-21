3df414cc7197d928f88cfefc4b91b720335d0471
mikebru <michael.bruner3@gmail.com>
2019-05-21 00:06:23 -0500

    added in AR core

---
+++ yARd/Assets/Scripts/Utility/trackingPoints.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackingPoints : MonoBehaviour
+{
+
+    public SpriteRenderer[] imageTransforms;
+    public Vector3[] orginOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageTransforms = GetComponentsInChildren<SpriteRenderer>();
+
+        orginOffset = new Vector3[imageTransforms.Length];
+
+        for(int i=0; i < imageTransforms.Length; i++)
+        {
+            orginOffset[i] = transform.position - imageTransforms[i].transform.position;
+        }
+
+    }
+
+    private void OnValidate()
+    {
+        Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
